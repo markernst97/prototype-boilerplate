ae3a893a35f4f9ba887d7f30819583986a115741
test <test@tests-MacBook-Air.local>
2020-09-29 13:36:29 +0200

    removed setup 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './setup/index.css';
-import App from './setup/App';
 import Workshop from './Workshop';
 
 // var ajax = new XMLHttpRequest();
